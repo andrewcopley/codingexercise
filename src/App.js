04a13fe5a492bede23a0273aa256a6d283b65c84
AJ Copley <ajcopley@AJs-MacBook-Pro.local>
2019-01-27 16:04:20 -0700

    Initial work
    Signed-off-by: AJ Copley <ajcopley@AJs-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      data: [],
+      pictures: [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://hoopla-ws-dev.hoopladigital.com/kinds/7/titles/featured?offset=0&limit=51&kindId=7', {
+      headers: {
+        'ws-api': '2.1'
+      }
+    })
+      .then((response) => {
+        console.log('RESPONSE: ', response)
+        this.setState({ data: response.data })
+        this.setState({ username: response.data[0].title })
+        console.log('YYYYYYYYY: ', this.state.data)
+        this.state.data.map((function (item) {
+          axios.get(`https://d2snwnmzyr8jue.cloudfront.net/${item.artKey}_270.jpeg`)
+            .then((response) => {
+              console.log('PICTURE RESPONSE: ', response)
+              this.state.pictures.push(`https://d2snwnmzyr8jue.cloudfront.net/${item.artKey}_270.jpeg`)
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        }));
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const info = this.state.data.map(function (item) {
+      return <div class="grid-item">
+        <img src={`https://d2snwnmzyr8jue.cloudfront.net/${item.artKey}_270.jpeg`} ></img>
+      <li> Title: {item.title} Artist: {item.artistName} </li>
       </div>
-    );
+    });
+    return (
+      <div class="grid-container">
+        <ul>
+          {info}
+        </ul>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
